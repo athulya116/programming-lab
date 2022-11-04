0cb05f650d673fbbfb82bb78f04008d4ca9683da
athulya116 <116333075+athulya116@users.noreply.github.com>
2022-11-04 10:43:01 +0530

    Add files via upload

---
+++ list_comprehension.py
@@ -0,0 +1,13 @@
+a=[3,5,-4,-7,-2,0,4,1]
+d=[n for n in a if n>=0]
+print(d)
+word=input(" enter a word ")
+vowel="aeiouAEIOU"
+vowel_list=[each for each in word if each in vowel]
+print(vowel_list)
+b=[1,5,6,8,6]
+c=[n*n for n in b ]
+print(c)
+e=input("enter a word")
+f=[ord(c) for c in e]
+print(f)
