dd2355ba3be0982bf2967ef988df3d66131150ab
athulya116 <116333075+athulya116@users.noreply.github.com>
2022-12-09 09:44:08 +0530

    Delete ex-4.py

--- cycle2/ex-4.py
+++
@@ -1,9 +0,0 @@
-def pyramid(n):
-    for i in range(1,n+1):
-        n1=i
-        for j in range(0,i):
-            print(n1,end=" ")
-            n1=n1+1
-        print("\r")
-a=int(input("enter a number"))
-pyramid(a)
